692dcdbc534faec9bf8542f069f65413d8f0b9c4
velagana <116190024+velagana@users.noreply.github.com>
2023-10-07 12:47:53 +0530

    Add files via upload

---
+++ cubes.py
@@ -0,0 +1,5 @@
+''' programme for finding the cubes of natural numbers '''
+'''S = [n2 (n + 1)2]/4,'''
+n = int(input("enter the value of n : "));
+result = (n ** 2 * (n + 1) ** 2) / 4
+print(result);
